e37631bb16538edbfff5aa06c13f3f559625b4cf
Caio César <caiogalaxy00@gmail.com>
2024-09-11 11:21:38 -0300

    Primeiro commit do projeto bancoapi

---
+++ src/test/java/com/caio/bancoapi/BancoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.bancoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
